673016b508aec4eaf71441eea66b2fb8de376704
Jefero95 <116748814+Jefero95@users.noreply.github.com>
2023-06-05 12:14:06 -0400

    We add the project

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x38f135108715Cc9689c58521555F9E0089407bba";
+export const tokenContractAddress =
+  "0xf22218E24976738bAbe7e5a242b180038F3d028b";
+export const stakingContractAddress =
+  "0x8FC90413E460a0245c10a6339D11902d38edD0ad";
